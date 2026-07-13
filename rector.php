998926c23722d00b09d7afb4d5d14d5bf04fb500
Alex Howansky <alex.howansky@gmail.com>
2026-07-13 19:06:09 -0400

    Composer bump.

--- rector.php
+++ rector.php
@@ -11,6 +11,6 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/tests',
     ]);
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
+        LevelSetList::UP_TO_PHP_82,
     ]);
 };
